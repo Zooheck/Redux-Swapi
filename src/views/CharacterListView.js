910435d3607b9ad6926972983126ace4a97b7050
Zooheck <davidscottflack@gmail.com>
2019-01-30 17:26:41 -0600

    Fix character mapStateToProps state bug

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
 // import actions
-
+import { getCharacters } from '../actions/index'
 class CharacterListView extends React.Component {
   constructor() {
     super();
@@ -11,10 +11,11 @@ class CharacterListView extends React.Component {
 
   componentDidMount() {
     // call our action
+    this.props.getCharacters()
   }
 
   render() {
-    if (this.props.fetching) {
+    if (this.props.isLoading) {
       // return something here to indicate that you are fetching data
     }
     return (
@@ -25,11 +26,17 @@ class CharacterListView extends React.Component {
   }
 }
 
+const mapStateToProps = (state) => ({
+  characters: state.charsReducer.characters,
+  isLoading: state.charsReducer.isLoading,
+  error: state.charsReducer.error
+})
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
 export default connect(
-  null /* mapStateToProps replaces null here */,
+  mapStateToProps /* mapStateToProps replaces null here */,
   {
     /* action creators go here */
+    getCharacters
   }
 )(CharacterListView);
